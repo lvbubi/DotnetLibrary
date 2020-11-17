f140cd557201637f2759e6db7b2c41157ffec5e9
buboribencebarnabas <buboribencebarnabas@lechnerkozpont.hu>
2020-11-17 23:29:17 +0100

    Migrations

--- netcore_gyakorlas/Models/Author.cs
+++ netcore_gyakorlas/Models/Author.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace netcore_gyakorlas.Models
+﻿namespace netcore_gyakorlas.Models
 {
     public class Author : AbstractEntity
     {
---
+++ netcore_gyakorlas/Models/Library.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace netcore_gyakorlas.Models
+{
+    public class Library : AbstractEntity
+    {
+        [ForeignKey("Book")]
+        public int BookId { get; set; }
+        public Book Book { get; set; }
+        public int Count { get; set; }
+    }
+}
