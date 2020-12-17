7b60b78b5c08e0b8ac475b9550646f27094de30e
buboribencebarnabas <buboribencebarnabas@lechnerkozpont.hu>
2020-12-17 23:21:13 +0100

    Websocker finito

--- netcore_gyakorlas/Middleware/BookUploaderWebsocket.cs
+++ netcore_gyakorlas/Middleware/BookUploaderWebsocket.cs
@@ -1,13 +1,16 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net.WebSockets;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using EventApp.Models.Communication;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Primitives;
 using netcore_gyakorlas.Models;
 using netcore_gyakorlas.Services;
 using Newtonsoft.Json.Linq;
+using JsonSerializer = System.Text.Json.JsonSerializer;
 
 namespace netcore_gyakorlas.Middleware
 {
@@ -57,23 +60,50 @@ namespace netcore_gyakorlas.Middleware
 
                 JArray books = JArray.Parse(mes);
                 
-                byte[] list = Encoding.UTF8.GetBytes("punci");
-                await webSocket.SendAsync(new ArraySegment<byte>(list, 0, list.Length), result.MessageType, result.EndOfMessage, CancellationToken.None);
                 
-                foreach (var jToken in books)
+                byte[] list = Encoding.UTF8.GetBytes("uploading...");
+                await webSocket.SendAsync(new ArraySegment<byte>(list, 0, list.Length), result.MessageType, result.EndOfMessage, CancellationToken.None);
+
+                IEnumerable<Book> bookList = books.Select(x => x.ToObject<Book>());
+                
+                foreach (var book in bookList)
                 {
-                    Book book = jToken.ToObject<Book>();
                     //_bookService.Create(book);
                     Console.WriteLine(book);
                 }
-
-                //books.Count;
-
-                //await webSocket.SendAsync(new ArraySegment<byte>(buffer, 0, result.Count), result.MessageType, result.EndOfMessage, CancellationToken.None);
-
+                
+                await SendResponse(webSocket, bookList);
+                await SendCloseEvent(webSocket);
+                
                 result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
             }
             await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
+        }
+
+
+        private async Task SendResponse(WebSocket webSocket, IEnumerable<Book> bookList)
+        {
+            
+            byte[] list = Encoding.UTF8.GetBytes("Count: " + bookList.Count());
+            await webSocket.SendAsync(new ArraySegment<byte>(list, 0, list.Length), WebSocketMessageType.Text, true, CancellationToken.None);
+            
+            byte[] dataAsBytes = bookList.SelectMany(book => Encoding.UTF8.GetBytes(
+                JsonSerializer.Serialize(new BookWsResponse
+                {
+                    Id = book.Id,
+                    Author = book.AuthorId.ToString(),
+                    Title = book.Title
+                }) + Environment.NewLine)
+            ).ToArray();
+            
+            await webSocket.SendAsync(new ArraySegment<byte>(dataAsBytes, 0, dataAsBytes.Length), WebSocketMessageType.Text, true, CancellationToken.None);
+        }
+        
+        private async Task SendCloseEvent(WebSocket webSocket)
+        {
+            byte[] list = Encoding.UTF8.GetBytes("close");
+            await webSocket.SendAsync(new ArraySegment<byte>(list, 0, list.Length), WebSocketMessageType.Text, true, CancellationToken.None);
+
         }
     }
 }
---
+++ netcore_gyakorlas/Models/Communication/BookWSResponse.cs
@@ -0,0 +1,9 @@
+namespace EventApp.Models.Communication
+{
+    public class BookWsResponse
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Id { get; set; }
+    }
+}
